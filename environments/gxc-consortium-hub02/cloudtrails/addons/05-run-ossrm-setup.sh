5b1c1cc7dfd903a7c451f0709b3d4d38132dbb36
Dwi Fahni Denni <dwi.denni@xapiens.id>
2025-03-24 11:09:13 +0700

    Add script for running ossrm setup

---
+++ environments/gxc-consortium-hub02/cloudtrails/addons/05-run-ossrm-setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+python ossrm-setup.py \
+  --endpoint "https://search-opensearch-222233334444-ezdw7b4jcnjyrmsue6tc3x75n4.ap-southeast-3.es.amazonaws.com" \
+  --username "gxc-admin" \
+  --password "GXC4dm1n-S3cr3t" \
+  --role-arn "arn:aws:iam::222233334444:role/opensearch-master-role" \
+  --no-verify-ssl
+
+sleep 5
+
+## Daily Snapshot
+python ossrm.py \
+  --endpoint "https://search-opensearch-222233334444-ezdw7b4jcnjyrmsue6tc3x75n4.ap-southeast-3.es.amazonaws.com" \
+  --region "ap-southeast-3" \
+  --repository "genomic-snapshot-daily" \
+  --bucket "genomic-snapshot-222233334444" \
+  --base-path "snapshots/daily" \
+  --role-arn "arn:aws:iam::222233334444:role/opensearch-snapshot-role" \
+  --master-role-arn "arn:aws:iam::222233334444:role/opensearch-master-role" \
+  --no-verify-ssl
+
+sleep 5
+
+## Weekly Snapshot
+python ossrm.py \
+  --endpoint "https://search-opensearch-222233334444-ezdw7b4jcnjyrmsue6tc3x75n4.ap-southeast-3.es.amazonaws.com" \
+  --region "ap-southeast-3" \
+  --repository "genomic-snapshot-weekly" \
+  --bucket "genomic-snapshot-222233334444" \
+  --base-path "snapshots/weekly" \
+  --role-arn "arn:aws:iam::222233334444:role/opensearch-snapshot-role" \
+  --master-role-arn "arn:aws:iam::222233334444:role/opensearch-master-role" \
+  --no-verify-ssl
+
+sleep 5
+
+## Monthly Snapshot
+python ossrm.py \
+  --endpoint "https://search-opensearch-222233334444-ezdw7b4jcnjyrmsue6tc3x75n4.ap-southeast-3.es.amazonaws.com" \
+  --region "ap-southeast-3" \
+  --repository "genomic-snapshot-monthly" \
+  --bucket "genomic-snapshot-222233334444" \
+  --base-path "snapshots/monthly" \
+  --role-arn "arn:aws:iam::222233334444:role/opensearch-snapshot-role" \
+  --master-role-arn "arn:aws:iam::222233334444:role/opensearch-master-role" \
+  --no-verify-ssl
