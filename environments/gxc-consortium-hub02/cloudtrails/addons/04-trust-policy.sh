b700e86720562281c298c0632255700979dfdaa6
Dwi Fahni Denni <dwi.denni@xapiens.id>
2025-04-11 11:07:26 +0700

    Restore IAM Role for assume-role OpenSearch Snapshot using global config arn:aws:sts::480756163420:assumed-role

--- environments/gxc-consortium-hub02/cloudtrails/addons/04-trust-policy.sh
+++ environments/gxc-consortium-hub02/cloudtrails/addons/04-trust-policy.sh
@@ -17,6 +17,13 @@ cat > trust-policy.json << 'EOF'
     {
       "Effect": "Allow",
       "Principal": {
+        "AWS": "arn:aws:sts::480756163420:assumed-role/cp-sts-grant-role/swift-ap-southeast-3-prod-127214202110"
+      },
+      "Action": "sts:AssumeRole"
+    },
+    {
+      "Effect": "Allow",
+      "Principal": {
         "AWS": "arn:aws:iam::127214202110:root"
       },
       "Action": "sts:AssumeRole"
